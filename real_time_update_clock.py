b602b8833f354a7691143c7190d7ac51bf8797e4
Sanchit Gupta <sanchitlogger@outlook.com>
2022-09-18 11:44:11 +0530

    proper

---
+++ real_time_update_clock.py
@@ -0,0 +1,10 @@
+
+import time
+import sys
+import datetime
+
+while True:
+    now = datetime.datetime.now()
+    print(now.strftime("%HH:%MM:%SS"), end="\r") 
+    sys.stdout.flush() # everyting is fuffers to terminal
+    time.sleep(0.1) # prints after every 0.1 seconds
