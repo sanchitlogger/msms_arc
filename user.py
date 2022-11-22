d42d7e50fc2322bd2cb35f4fe0dda2f094c26b2e
Sanchit Gupta <sanchitlogger@outlook.com>
2022-11-22 16:34:40 +0000

    upgraded user.py

--- user.py
+++ user.py
@@ -1,22 +1,37 @@
 # This program will be used to create new users and add them to the database
 from cryptography import fernet as fer
-import pickle
 import mysql.connector as con
+import getpass as pas
 
 with con.connect(host="sanchitlogger-vm1.centralindia.cloudapp.azure.com",user="sanchitlogger",password="Password@1234",database="msms") as db:
     cur = db.cursor()
     def create_user():
         f_name = input("Enter first name: ")
         l_name = input("Enter last name: ")
-        username = input("Enter username: ")
-        password = input("Enter password: ")
         gender = input("Enter gender(M/F): ").lower()
-        key = fer.Fernet.generate_key()
-        fernet = fer.Fernet(key)
-        encrypted = fernet.encrypt(password.encode())
-        cur.execute("INSERT INTO users (f_name , l_name, username, gender, pass, secret) VALUES (%s, %s, %s, %s, %s, %s)", (f_name, l_name,username, gender, encrypted, key))
-        db.commit()
-        print("User created successfully")
-    
+        username = input("Enter username: ")
+        a = 1
+        while a<=4:
+            password1 = pas.getpass(prompt="Enter password: ")
+            password2 = pas.getpass(prompt="Renter password: ")
+            if password1 == password2:
+                key = fer.Fernet.generate_key()
+                fernet = fer.Fernet(key)
+                encrypted = fernet.encrypt(password1.encode())
+                cur.execute("INSERT INTO users (f_name , l_name, username, gender, pass, secret) VALUES (%s, %s, %s, %s, %s, %s)", (f_name, l_name,username, gender, encrypted, key))
+                b = db.commit()
+                print("User created successfully")
+                print(b)
+            else:
+                print("Wrong Password. Try again!")
+                a +=1
+            #if a ==5:
+            
+create_user()            
+        
+        
+"""
     def user_login():
-
+        username = input("Enter username:")
+        cur.execute("")
+"""
